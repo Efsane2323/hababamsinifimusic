be58741136ee7cfdd316913a96278abff2bf0e53
Mehmettt <87609530+Mehmetgul0202@users.noreply.github.com>
2021-08-16 18:30:16 +0300

    Update chat_member_updated.py

--- handlers/chat_member_updated.py
+++ handlers/chat_member_updated.py
@@ -9,9 +9,12 @@ async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
     chat = chat_member_updated.chat.id
     new = chat_member_updated.new_chat_member
 
-    if new.can_manage_voice_chats:
-        if new.user.id not in cache.admins[chat]:
-            cache.admins[chat].append(new.user.id)
-    else:
-        if new.user.id in cache.admins[chat]:
-            cache.admins[chat].remove(new.user.id)
+    try:
+        if new.can_manage_voice_chats:
+            if new.user.id not in cache.admins[chat]:
+                cache.admins[chat].append(new.user.id)
+        else:
+            if new.user.id in cache.admins[chat]:
+                cache.admins[chat].remove(new.user.id)
+    except:
+        pass
