2b3b2314f55b64f2b1fa29642c79ee6c4720c6b2
Mehmettt <87609530+Mehmetgul0202@users.noreply.github.com>
2021-08-16 18:22:34 +0300

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -45,7 +45,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("İndiriliyor...")
+    m.edit("📥İndiriliyor...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
