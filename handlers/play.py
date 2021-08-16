e622f35cfadc020c434e7600569026df04ec5119
Mehmettt <87609530+Mehmetgul0202@users.noreply.github.com>
2021-08-16 16:08:36 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -3,7 +3,7 @@ from os import path
 from pyrogram import Client
 from pyrogram.types import Message, Voice
 
-from callsmusic import callsmusic, queues
+import callsmusic
 
 import converter
 from downloaders import youtube
@@ -13,34 +13,18 @@ from helpers.filters import command, other_filters
 from helpers.decorators import errors
 from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(command("play") & other_filters)
+
+@Client.on_message(command("oynat") & other_filters)
 @errors
-async def play(_, message: Message):
-
-    lel = await message.reply("🔄 **Processing** sounds...")
-    sender_id = message.from_user.id
-    sender_name = message.from_user.first_name
-
-    keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="🔊 Channel",
-                        url="https://t.me/Infinity_BOTs")
-                   
-                ]
-            ]
-        )
-
+async def oynat(_, message: Message):
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
     url = get_url(message)
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed to play!"
+                f"**{bn} :-** 😕 Ses Dosyası Uzun {DURATION_LIMIT} minute(s) izin verilmez!\n🤐 Sağlanan ses, {audio.duration / 60} minute(s)"
             )
 
         file_name = get_file_name(audio)
@@ -51,18 +35,10 @@ async def play(_, message: Message):
     elif url:
         file_path = await converter.convert(youtube.download(url))
     else:
-        return await lel.edit_text("❗ You did not give me anything to play!")
+        return await message.reply_text(f"**{bn} :-** 🙄 Bana oynatacak bir şey vermedin.!")
 
     if message.chat.id in callsmusic.pytgcalls.active_calls:
-        position = await queues.put(message.chat.id, file=file_path)
-        await lel.edit(f"#⃣ **Queued** at position {position}!")
+        await message.reply_text(f"**{bn} :-** 😉 Sıraya Alındı. Sırası= #{await callsmusic.queues.put(message.chat.id, file_path=file_path)} !")
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
-        await message.reply_photo(
-        photo="https://telegra.ph/file/a4fa687ed647cfef52402.jpg",
-        reply_markup=keyboard,
-        caption="▶️ **Playing** here the song requested by {}!".format(
-        message.from_user.mention()
-        ),
-    )
-        return await lel.delete()
+        await message.reply_text(f"**{bn} :-** 🥳 Oynatılıyor...")
